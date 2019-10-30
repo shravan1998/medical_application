795ee27e9287553d583ef4076c62ff35d59d05ea
shravan1998 <shrkum51@gmail.com>
2019-10-30 14:04:02 +0530

    Added ng form

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const mysql = require("mysql");
+const bodyparser = require("body-parser");
 
 const app = express();
+const connection = mysql.createConnection({
+    host: "localhost",
+    username: "root",
+    password: "",
+    database:"medical_application"
+});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/'));
 
 app.listen(8000,function(){
     console.log("Runs at 8000");
--- medical-application/src/app/app.module.ts
+++ medical-application/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,7 @@ import { PurchaseComponent } from './purchase/purchase.component';
   declarations: [
     AppComponent,
     HomeComponent,
-   
+  
     DashboardComponent,
     SalesInvoiceComponent,
     DebitNoteComponent,
@@ -24,7 +25,8 @@ import { PurchaseComponent } from './purchase/purchase.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
