2469f6e764c0eff75addff8ad4a6cee988d28a3d
shravan1998 <shrkum51@gmail.com>
2019-11-07 09:30:01 +0530

    added mathematical formula

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const bodyparser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const connection = mysql.createConnection({
@@ -12,6 +13,8 @@ const connection = mysql.createConnection({
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname+'/'));
+app.use(cors());
+
 
 app.listen(8000,function(){
     console.log("Runs at 8000");
--- medical-application/src/app/sales-invoice/sales-invoice.component.ts
+++ medical-application/src/app/sales-invoice/sales-invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {InvoiceService} from '../invoice.service';
 
 @Component({
   selector: 'app-sales-invoice',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-invoice.component.css']
 })
 export class SalesInvoiceComponent implements OnInit {
+  master:String;
+  item:String;
+  itemname:String;
+  branch:String;
+  expiry:String;
+  disc:Number;
+  quantity:number;
+  salesrt:number;
+  mrpbox:Number;
+  cgst:Number;
+  sgst:Number;
+  amount:Number;
+  dosage:Number;
+  doctorid:String;
+  customerid:String;
+  payment:String;
 
-  constructor() { }
+  
+
+  constructor(private invoiceservice:InvoiceService) { }
 
   ngOnInit() {
-  }
+    
 
+  }
+  onSave(item){
+
+  }
+  onCal(disc,quantity,mrp,event){
+    event.target.value=(mrp.value-((mrp.value)*(disc.value/100)))*quantity.value;
+    
+  }
+  NetAmount(disc,quantity,mrp,cgst,sgst,event){
+    event.target.value=Math.round((mrp.value-((mrp.value)*(disc.value/100))+((mrp.value)*(cgst.value/100))
+    +(mrp.value)*(sgst.value/100))*quantity.value);
+    
+  }
+ 
 }
